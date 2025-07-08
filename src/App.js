ef6c292d367b7e2e90b569e40e888923f2fd3944
sweathkumar <sweathkumar@cmots.com>
2025-07-08 16:46:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <nav>
+        <div class="navItem">
+            <img class="navLogo" height="30px" src="./resources/logo.svg" alt=""/>
+            <p class="brand">Smart CV</p>
+        </div>
+        <div class="navItem">
+            <button>Get Started</button>
+        </div>
+    </nav>
+
+        <section class="section-one">
+      <div>
+        <div>
+          <p class="head-light text-center">
+            Increase Chance of getting <br />
+            selected with <span>Smart CV</span>
+          </p>
+          <p class="desc-light text-center">
+            Let AI Do Your Resume Work - You Prep Your Interview
+          </p>
+          <div>
+            <button>Go to playground</button>
+          </div>
+          <img class="resume-temp" src="./resources/resume.svg" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <hr/>
+
+    <section class="section-one">
+      <div>
+      </div>
+    </section>
     </div>
   );
 }
