172ef8e02ec99002d8a500911424ec2466b8e27d
sweathkumar <sweathkumar@cmots.com>
2025-07-08 17:03:42 +0530

    2 commit

--- src/App.js
+++ src/App.js
@@ -16,15 +16,17 @@ function App() {
         <section class="section-one">
       <div>
         <div>
-          <p class="head-light text-center">
-            Increase Chance of getting <br />
-            selected with <span>Smart CV</span>
-          </p>
-          <p class="desc-light text-center">
-            Let AI Do Your Resume Work - You Prep Your Interview
-          </p>
           <div>
-            <button>Go to playground</button>
+            <p class="head-light text-center">
+              Increase Chance of getting <br />
+              selected with <span>Smart CV</span>
+            </p>
+            <p class="desc-light text-center">
+              Let AI Do Your Resume Work - You Prep Your Interview
+            </p>
+            <div>
+              <button>Go to playground</button>
+            </div>
           </div>
           <img class="resume-temp" src="./resources/resume.svg" alt="" />
         </div>
